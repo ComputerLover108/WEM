375a67022c15d282612e4b0f44c5074af8fd44c8
ComputerLover108 <ComputerLover108@gmail.com>
2016-06-15 18:05:05 +0800

    通用视图添加搜索功能

--- test/HLDT/ProcessProduction/views.py
+++ test/HLDT/ProcessProduction/views.py
@@ -4,6 +4,27 @@ from datetime import date
 from .models import 生产信息
 from .ReportFormData import ProductionDailyData
 # Create your views here.
+def index(request):
+    title='工艺生产'
+    用户='游客'
+    数据采集 = (
+    ('配产数据','Proration'),
+    ('快速录入','QuickInput'),
+    ('装车数据','OutputData'),
+    ('海管数据','SeaPipeData'),
+    ('化验数据','TestData'),
+    )
+    报表 = (
+    ('海管报表','SeaPipeReport'),
+    ('装车报表','OutputReport'),
+    ('化验报表','TestReport'),
+    ('生产动态','ProductionStatus'),
+    ('生产日报','DailyProduction'),
+    ('生产月报','MonthlyProduction'),
+    ('生产年报','AnnualProduction'),
+    )
+    return render(request, 'ProcessProduction/index.html', locals())
+
 def ProductionAnnual(request,year):
 	return render(request,'ProcessProduction/ProductionAnnual.html', locals())
 
--- test/HLDT/QHSE/views.py
+++ test/HLDT/QHSE/views.py
@@ -5,14 +5,19 @@ from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import WorkPhone
-
+from django.db.models import Q
 
 class WorkPhoneList(ListView):
-    # template_name = "WorkPhone_list.html"
+    template_name = "WorkPhone_list.html"
     model = WorkPhone
     context_object_name = 'WorkPhone-list'    
-    queryset = WorkPhone.objects.all().order_by('地点')
     paginate_by = 10
+    def get_queryset(self):
+    	queryset = WorkPhone.objects.all().order_by('地点','电话')
+    	keyword = self.request.GET.get('keyword')
+    	if keyword :
+    		queryset = WorkPhone.objects.filter(Q(地点__icontains=keyword)|Q(电话__icontains=keyword)|Q(备注__icontains=keyword)).order_by('地点','电话')
+    	return queryset   
 
 class WorkPhoneCreate(CreateView):
 	model = WorkPhone
