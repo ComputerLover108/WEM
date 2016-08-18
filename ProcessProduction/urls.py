04775d7e9b9a6ff02a486bb38ac383acaa27062e
wkx <wkx@MyServer.ComputerLover.help>
2016-08-18 21:29:30 +0800

    json learn

--- ProcessProduction/urls.py
+++ ProcessProduction/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ProductionDaily',views.ProductionDaily),
-    url(r'^ProductionData',views.ProductionData),
+    url(r'^ProductionData',views.ProductionData,name='ProductionData'),
+    url(r'^ProductionDataList',views.ProductionDataList,name='ProductionDataList'),
+    url(r'^ProductionStatus',views.ProductionStatus,name='ProductionData'),
     url(r'^ReportForm/Production/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$',views.ProductionDaily),
     url(r'^ReportForm/Production/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$',views.ProductionMonthly),
     url(r'^ReportForm/Production/(?P<year>[0-9]{4})/$',views.ProductionAnnual),
--- ProcessProduction/views.py
+++ ProcessProduction/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.db.models import Q
-from datetime import date
+from datetime import date,datetime,time
 from .models import 生产信息
 from .ReportFormData import ProductionDailyData
 from django.views.generic import ListView
 from django.db.models import Q
 from django.db import connection
-
+from django.http import JsonResponse
 # Create your views here.
 def index(request):
     title='工艺生产'
@@ -22,53 +23,75 @@ def index(request):
     ('海管报表','SeaPipeReport'),
     ('装车报表','OutputReport'),
     ('化验报表','TestReport'),
-    ('生产信息','ProductionData'),
-    ('生产动态','ProductionStatus'),
+    ('生产信息','ProductionDataList'),
+    ('生产动态','ProductionStatusList'),
     ('生产日报','ProductionDaily'),
     ('生产月报','MonthlyProduction'),
     ('生产年报','AnnualProduction'),
     )
     return render(request, 'ProcessProduction/index.html', locals())
 
-class ProductionDataList(ListView):
-    template_name = "QHSE/ProductionData.html"
-    model = 生产信息
-    context_object_name = 'ProductionData'
-    paginate_by = 10
-    def get_queryset(self):
-        queryset = 生产信息.objects.all()
-        keyword = self.request.GET.get('keyword')
-        if keyword :
-            queryset = 生产信息.objects.filter(Q(地点__icontains=keyword)|Q(电话__icontains=keyword)|Q(备注__icontains=keyword)).order_by('地点','电话')
-        else:
-            queryset = 生产信息.objects.all()
-        return queryset
-    def get_context_data(self, **kwargs):
-        context = super(ProductionData, self).get_context_data(**kwargs)
-        context['Title'] = "生产信息"
-        return context
-
-def ProductionData(request):
+def ProductionDataList(request):
     Title = "生产信息"
     tableName = "生产信息"
-    # columnName = [
-    #     {title:"日期"},
-    #     {title:"名称"},
-    #     {title:"单位"},
-    #     {title:"数据"},
-    #     {title:"类别"},
-    #     {title:"状态"},
-    #     {title:"备注"},
-    #     {title:"月累"},
-    #     {title:"年累"}
-    # ]
+    columns = ['日期','名称','单位','数据','类别','状态','备注','月累','年累']
+    return render(request,'ProcessProduction/ProductionDataList.html',locals())
+
+def ProductionData(request):
     cursor = connection.cursor()
-    cursor.execute("select 日期,名称,单位,数据,类别,状态,备注,月累,年累 from 生产信息 ;")
-    data = cursor.fetchall()
-    return render(request,'ProcessProduction/ProductionData.html',locals())
+    cursor.execute("select 日期,名称,单位,数据,类别,状态,备注,月累,年累 from 生产信息 limit 9;")
+    rows = cursor.fetchall()
+    record = list()
+    data = list()
+    for row in rows:
+        for field in row:
+            if isinstance(field,(date,datetime,time)):
+                # record.append(field.strftime('%Y-%m-%d'))
+                record.append(field.isoformat())
+                continue
+            if isinstance(field,(int,float)):
+                record.append(str(field))
+                continue
+            if field :
+                record.append(field)
+            else:
+                record.append('')
+        data.append(record)
+    # print(data)
+    # data = json.dumps(data,indent=4)
+    return JsonResponse(data, safe=False)
+
+def ProductionStatusList(request):
+    Title = "生产动态"
+    tableName = "生产动态"
+    columns = ['时间','名称','单位','数据','类别','备注']
+    return render(request,'ProcessProduction/ProductionStatusList.html',locals())
 
 def ProductionStatus(request):
-    pass
+    Title = "生产动态"
+    tableName = "生产动态"
+    cursor = connection.cursor()
+    cursor.execute("select 时间,名称,单位,数据,类别,备注 from 生产动态 limit 36;")
+    rows = cursor.fetchall()
+    record = list()
+    data = list()
+    for row in rows:
+        for field in row:
+            if isinstance(field,(date,datetime,time)):
+                # record.append(field.strftime('%Y-%m-%d %'))
+                record.append(field.isoformat())
+                continue
+            if isinstance(field,(int,float)):
+                record.append(str(field))
+                continue
+            if field :
+                record.append(field)
+            else:
+                record.append('')
+        data.append(record)
+    # print(data)
+    # data = json.dumps(data,indent=4)
+    return JsonResponse(data, safe=False)
 
 def ProductionAnnual(request,year):
 	return render(request,'ProcessProduction/ProductionAnnual.html', locals())
