7fd9be9daad0fffdcc138de61077f9c9fbdc1d7f
wkx <wkx@MyServer.ComputerLover.help>
2016-12-04 22:35:09 +0800

    继续重写生产日报

--- ProcessProduction/productionDaily.py
+++ ProcessProduction/productionDaily.py
@@ -19,94 +19,99 @@ def getProductionDailyData(date):
     getDerivedData(data,date) 
     return data
 
-def getBaseData(data,date): 
-    getDistributionData(data,date)
-    getUpstreamData(data,date)
-    getSeaPipeData(data,date)
-    getGasData(data,date)
-    getOilData(data,date)
-    getHydrocarbonData(data,date)
-    getChemicalsData(data,date)
-    getWaterData(data,date)
-    getPowerData(data,date)
-    getRemark(data,date)
-
-# 配产
-def getDistributionData(data,date):
+def getBaseData(data,date):
     cursor = connection.cursor()
-    SQL = "select 日期,名称,单位,数据,类别,状态,备注 from 生产信息 where 日期=%s and 状态=%s"
-    state = '计划'
-    args = [date,state]
+    SQL = "select 日期,名称,单位,数据,类别,状态,备注,月累,年累,数据源 from 生产信息 where 日期=%s "
+    args = [date]
     cursor.execute(SQL,args)
     rows = cursor.fetchall()
+
+    getDistributionData(data,date,rows)
+    getUpstreamData(data,date,rows)
+    getSeaPipeData(data,date,rows)
+    getGasData(data,date,rows)
+    getOilData(data,date,rows)
+    getHydrocarbonData(data,date,rows)
+    getChemicalsData(data,date,rows)
+    getWaterData(data,date,rows)
+    getPowerData(data,date,rows)
+    getRemark(data,date,rows)
+
+# 配产
+def getDistributionData(data,date,rows):
+    state = '计划'
     for row in rows :
-        key = row[1] + row[2]
-        value = row[3]
-        data[key] = value
+        if row[5] == state :
+            data[row[1]+row[2]] = row[3]
 
 # 海管
-def getSeaPipeData(data,date):
-    pass
+def getSeaPipeData(data,date,rows):
+    category = '海管'
+    for row in rows :
+        if row[4] == category :
+            if row[6] :
+                if re.search(row[6],row[1]):
+                    data[row[1]+row[2]] = row[3]
+                else:
+                    data[row[1]+row[6]+row[2]] = row[3]
+    pattern = r'海管[进出]口(压力兆帕|温度摄氏度)'
+    for key in data :
+        if re.match(pattern,key):
+            data[key] = data[key] if data[key] else '-'
+    data['海管进出口压力兆帕'] = '{0}/{1}'.format(data['海管进口压力兆帕'],data['海管出口压力兆帕'])
+    data['海管进出口温度摄氏度'] = '{0}/{1}'.format(data['海管进口温度摄氏度'],data['海管出口温度摄氏度'])
 
 # 上游
-def getUpstreamData(data,date):
-    category = '上游'
-    records = 生产信息.objects.filter(Q(日期=date),Q(类别=category))
-    for record in records:
-        if re.match('JZ20-2凝析油',record.名称) :
-            name = record.名称.replace('-','')
-            if record.单位 == '方' :
-                name = name+'数据'
-                context[name] = record.数据
-            if re.match('吨/立方米',record.单位) :
-                name = name
-#                print(name,record.数据)
-                context[name] = record.数据
+def getUpstreamData(data,date,rows):
+    names = ['JZ20-2体系','JZ20-2凝析油','JZ25-1S原油']
+    for row in rows:
+        if row[1] in names:
+            key = (row[1]+row[5]+row[2]).replace('-','')
+            data[key] = row[3]
 
 # 天然气
-def getGasData(data,date):
+def getGasData(data,date,rows):
     names = (
         '稳定区',
         '入厂计量',
         # '锦天化',
+        '精细化工CNG',
         '精细化工',
         '污水处理厂',
         '新奥燃气',
         '自用气'
     )
     category = '天然气'    
-    cursor = connection.cursor()
-    SQL = "select 日期,名称,单位,数据,类别,状态,备注 from 生产信息 where 日期=%s  and 名称 in %s and 类别=%s"
-    args = [date,names,category]
-    cursor.execute(SQL,args)
-    rows = cursor.fetchall()
     for row in rows :
-        key = row[1] + row[2]
-        value = row[3]
-        data[key] = value    
+        if (row[1] in names) and row[4]==category:
+            data[row[1]+row[2]] =row[3]    
 
 # 轻油
-def getOilData(data,date):
-    pass
+def getOilData(data,date,rows):
+    category = '轻油'
+    for row in rows :
+        if row[4] == category :
+            key = (row[1] + row[2]).replace('-','')
+            data[key] = row[3]
 
 # 轻烃
-def getHydrocarbonData(data,date):
+def getHydrocarbonData(data,date,rows):
     pass
 
 # 化学药剂
-def getChemicalsData(data,date):
+def getChemicalsData(data,date,rows):
     pass
 
 # 水
-def getWaterData(data,date):
+def getWaterData(data,date,rows):
     pass
 
 # 电
-def getPowerData(data,date):
+def getPowerData(data,date,rows):
     pass
 
 # 备注
-def getRemark(data,date):
+def getRemark(data,date,rows):
     pass
 
 # 相关数据
