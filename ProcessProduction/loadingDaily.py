0bc0d30b517d5bad7004e8b78805d246f40e8e26
ComputerLover108 <ComputerLover108@gmail.com>
2016-12-17 09:02:14 +0800

    loadingDaily continue

--- ProcessProduction/loadingDaily.py
+++ ProcessProduction/loadingDaily.py
@@ -7,17 +7,26 @@ logger = logging.getLogger('django')
 
 def getLoadingData(sd):
     data = dict()
-    dt = dict()
-    dbbl = dict()
     cursor = connection.cursor()
     SQL = "SELECT 客户名称,产品名称,sum(实际装车t),sum(实际装车bbl)  FROM 提单 WHERE 日期=%s GROUP BY 客户名称,产品名称 ORDER BY 客户名称,产品名称;"
     args = [sd]
     cursor.execute(SQL, args)
     rows = cursor.fetchall()
-    for row in rows:
-        key = row[0]
-        dt[row[1]] = row[2]
-        dbbl[row[1]] = row[3]
-        data[key] = dt
-        logger.info(key, dt, dbbl)
+    names = set()
+    names = [row[0] for row in rows]
+    logger.info(names)
+    for name in names:
+        dt = dict()
+        temp = list()
+        for row in rows:
+            if row[0] == name:
+                key = row[1] + 't'
+                dt[key] = row[2]
+                # logger.info(name, key, row[2])
+                if row[3]:
+                    key = row[1] + 'bbl'
+                    dt[key] = row[3]
+                    logger.info(name, key, row[3])
+        data[name] = dt
+        # logger.info(name, dt)
     return data
