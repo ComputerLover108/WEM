26fddf2ab6129ae2186e9541bdb6c2c7654664a7
ComputerLover108 <ComputerLover108@gmail.com>
2019-12-09 17:39:11 +0800

    learn docker

--- other/database/initHLD.sh
+++ other/database/initHLD.sh
@@ -1,23 +1,44 @@
-#!/bin/sh
-VERSION=10.0
-PGHOME=/opt/pgsql$VERSION
-PGDATA=/home/postgres/pgdata/$VERSION/MasterPG/data
-PGPASSWORD=pgpassword
-#PGPASSFILE=pgpass.conf
+#!/bin/bash
+VERSION=""
+# PGHOME=/opt/pgsql$VERSION
+PGDATA=""
+PGPASSWORD=""
 #PGBACKUP=/home/postgres/pgdata/backup
 PGHOST=localhost
 PGPORT=5432
 PGUSER=postgres
-PGADMINISTRATOR=ComputerLover
+PGADMINISTRATOR=""
 DATABASE=postgres
+while getopts v:d:u:w option ; do
+	case $option in
+		v)
+		VERSION=$OPTARG
+		;;		
+		d)
+		PGDATA=$OPTARG/$VERSION/MasterPG/
+		;;
+		u)
+		PGADMINISTRATOR=$OPTARG
+		;;
+		w)
+		PGPASSWORD=$OPTARG
+		;;
+	esac
+done
 
+if [ ! $PGADMINSTRATOR ]; then
+	PGADMINISTRATOR=ComputerLover
+fi
+
+if [ ! $PGPASSWORD ] ; then
+	PGPASSWORD=pgpassword
+fi
 export PATH=$PGHOME/bin:$PATH
 export LD_LIBRARY_PATH=$PGHOME/lib:$LD_LIBRARY_PATH
 
 pg_ctl -D  $PGDATA stop
 rm -rf $PGDATA
 initdb -E UTF8   -U $PGADMINISTRATOR --pwfile=$PGPASSWORD -D $PGDATA
-sed -i 's/#password_encryption = md5/password_encryption = scram-sha-256/' $PGDATA/postgresql.conf
 pg_ctl -D  $PGDATA start 
 sleep 9
 psql -U $PGADMINISTRATOR -f initHLD.sql  -d $DATABASE 
