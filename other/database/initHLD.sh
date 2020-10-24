73859e32b2371667e34aec5db8de2d2ed8768f71
wkx <wkx@homeServer.ComputerLover.help>
2020-10-25 07:36:26 +0800

    pgsql 安装脚本优化

--- other/database/initHLD.sh
+++ other/database/initHLD.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-VERSION=""
-# PGHOME=/opt/pgsql$VERSION
-PGDATA=""
-PGPASSWORD=""
+# VERSION=""
+# PGDATA=""
+# PGPASSWORD=""
 #PGBACKUP=/home/postgres/pgdata/backup
+TIMEOUT=3
 PGHOST=localhost
 PGPORT=5432
 PGUSER=postgres
-PGADMINISTRATOR=""
 DATABASE=postgres
-while getopts v:d:u:w option ; do
+while getopts v:d:w option ; do
 	case $option in
 		v)
 		VERSION=$OPTARG
+		PGHOME=/opt/pgsql$VERSION
 		;;		
 		d)
-		PGDATA=$OPTARG/$VERSION/MasterPG/
-		;;
-		u)
-		PGADMINISTRATOR=$OPTARG
+		PGDATA=$OPTARG/$VERSION/MasterPG/data
 		;;
 		w)
 		PGPASSWORD=$OPTARG
@@ -26,24 +23,24 @@ while getopts v:d:u:w option ; do
 	esac
 done
 
-if [ ! $PGADMINSTRATOR ]; then
-	PGADMINISTRATOR=ComputerLover
-fi
-
 if [ ! $PGPASSWORD ] ; then
 	PGPASSWORD=pgpassword
 fi
+
 export PATH=$PGHOME/bin:$PATH
 export LD_LIBRARY_PATH=$PGHOME/lib:$LD_LIBRARY_PATH
 
 pg_ctl -D  $PGDATA stop
 rm -rf $PGDATA
-initdb -E UTF8   -U $PGADMINISTRATOR --pwfile=$PGPASSWORD -D $PGDATA
+initdb -E UTF8  --pwfile=$PGPASSWORD -D $PGDATA
 pg_ctl -D  $PGDATA start 
-sleep 9
-psql -U $PGADMINISTRATOR -f initHLD.sql  -d $DATABASE 
+sleep $TIMEOUT
+psql -f initHLD.sql -d $DATABASE
 pg_ctl -D $PGDATA stop 
-sleep 9
-cp -v pg_hba.conf postgresql.conf $PGDATA/
+sleep $TIMEOUT
+cp -v $PGDATA/pg_hba.conf $PGDATA/pg_hba.bak
+cp -v $PGDATA/postgresql.conf $PGDATA/postgresql.bak
+cat pg_hba.conf >> $PGDATA/pg_hba.conf
+cp -v postgresql.conf $PGDATA
 cp -v pgpass.conf ~/.pgpass && chmod 0600 ~/.pgpass
-pg_ctl -D $PGDATA start
+# pg_ctl -D $PGDATA start
